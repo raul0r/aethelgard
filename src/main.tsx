fe48578cc7172f8ff22bed9577ce45f8fd698621
raul0r <raulbta0@gmail.com>
2026-03-18 17:48:24 -0500

    added missing main.tsx file

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './assets/textures/grimoire.css'
+
+// This connects the React 'App' to the 'root' div in your index.html
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
